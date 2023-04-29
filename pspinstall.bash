fe9373973344ee85a2665865b05f09d86c474019
williamblair <bblair12876@aol.com>
2023-04-29 13:55:28 -0400

    add build zlib to cmake flags

--- pspinstall.bash
+++ pspinstall.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf pspbuild
 mkdir pspbuild && cd pspbuild
-psp-cmake .. -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_NO_EXPORT=ON
+psp-cmake .. -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_NO_EXPORT=ON -DASSIMP_BUILD_ZLIB=ON
 find ./ -name '*.make' | xargs sed -i 's/\-fPIC//'
 make -j4 VERBOSE=1 install
 
